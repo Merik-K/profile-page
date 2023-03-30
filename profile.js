0b0fccb405a9f27047fc3106d0907f1985b8847f
Merik-K <110576661+Merik-K@users.noreply.github.com>
2023-03-29 23:03:57 -0700

    added all 15 jquery functions

--- profile.js
+++ profile.js
@@ -1,13 +1,25 @@
-// $('#menu').on('click', function () {
-//   $('#pfp').toggle(500);
-// });
-$('#menu').on('click', function () {
-  document.getElementById('main').style.marginLeft('0');
-  $('#pfp').hide(500);
-});
-$('#menu').on('click', function () {
-  document.getElementById('main').style.marginLeft('12.5rem');
-  $('#pfp').show(500);
+$(window).on('load', function () {
+  $('#tabs1').slideUp(1000).delay(200).slideDown(3000);
+  $('#tabs2').animate({ opacity: 0.8 }, 1000).animate({ opacity: 1 }, 2000);
 });
 
-$('#pfpicon');
+$('p').hover(function () {
+  $(this).append($('<span> lols </span>'));
+});
+
+$('nav').hover(
+  function () {
+    $(this).hide(5000).show(10);
+  },
+  function () {
+    $(this).find('hover').last().remove();
+  }
+);
+
+$('.pfpicon').on('click', function () {
+  $('#menu').toggle(500);
+  $('.main').css('marginLeft', '3.5rem').css('backgroundColor', 'none');
+});
+$(function () {
+  $('#menu, .tabs li');
+});
