a0d6659f5ace12e1d9dc6129e8e1d6dba6598905
MerikKuhn <mkuhn181@west-mec.org>
2023-03-29 17:57:17 -0700

    getting the page ready

--- profile.js
+++ profile.js
@@ -1,3 +1,13 @@
+// $('#menu').on('click', function () {
+//   $('#pfp').toggle(500);
+// });
 $('#menu').on('click', function () {
-  $('#pfp').toggle(500);
+  document.getElementById('main').style.marginLeft('0');
+  $('#pfp').hide(500);
 });
+$('#menu').on('click', function () {
+  document.getElementById('main').style.marginLeft('12.5rem');
+  $('#pfp').show(500);
+});
+
+$('#pfpicon');
